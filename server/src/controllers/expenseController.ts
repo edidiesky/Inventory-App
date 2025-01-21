887bc947cb61cafe2f997e58a4dcb109546e41cb
edidiesky <essienedidiong1000@gmail.com>
2025-01-21 03:14:21 -0800

    Making changes to the server

---
+++ server/src/controllers/expenseController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getExpensesByCategory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany(
+  
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving expenses by category" });
+  }
+};
